e4e0c5c9106f3c08dd85332e2c89ae85d9420a13
Anna Somova <anna.somova@khamac0001.kha.gameloft.org>
2013-07-09 13:37:04 +0300

    Initial Commit

---
+++ GLParticles1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GLParticles1
+//
+//  Created by Anna Somova on 7/9/13.
+//  Copyright (c) 2013 Anna Somova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GLParticles1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GLParticles1
+//
+//  Created by Anna Somova on 7/9/13.
+//  Copyright (c) 2013 Anna Somova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
